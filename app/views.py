397b0400bd8db99fe7a1ea0cf5140f988d02dd6f
Light <Lightheimer@gmail.com>
2024-02-07 04:21:54 -0800

    model user

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.db import models
 
 # Create your views here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    date_creation = models.DateField(auto_now_add=True)
+    # Autres champs ...
+
+    def __str__(self):
+        return self.username
